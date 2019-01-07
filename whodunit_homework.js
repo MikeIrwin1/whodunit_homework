c40111ee592b0eb453257b45a40c12b1005f64ba
Michael Irwin <mike.irwin1@gmail.com>
2019-01-07 17:04:49 +0000

    homework complete

---
+++ whodunit_homework.js
@@ -0,0 +1,184 @@
+// Episode 1
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Library',
+//   weapon: 'Rope'
+// };
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${scenario.murderer}.`;
+// }
+//
+// const verdict = declareMurderer();
+// console.log(verdict);
+// I think it will return 'Miss Scarlet' because the constants are created with block scope but they are all on the same scope level. So they can all see each other and the murderer from the scenario is Miss Scarlet.
+
+// Episode 2
+// const murderer = 'Professor Plum';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+// I think that the console will throw an error because we are trying to change a constant variable.
+
+// Episode 3
+// let murderer = 'Professor Plum';
+//
+// const declareMurderer = function() {
+//   let murderer = 'Mrs. Peacock';
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// const firstVerdict = declareMurderer();
+// console.log('First Verdict: ', firstVerdict);
+//
+// const secondVerdict = `The murderer is ${murderer}.`;
+// console.log('Second Verdict: ', secondVerdict);
+// the first verdict will return 'Mrs Peacock' because the function is called changing the murderer to Mrs Peacock and the murderer is called from within the block scope. the second verdict will return Professor Plum because the murderer has not been affected by the declareMurderer function
+
+// Episode 4
+// let suspectOne = 'Miss Scarlet';
+// let suspectTwo = 'Professor Plum';
+// let suspectThree = 'Mrs. Peacock';
+//
+// const declareAllSuspects = function() {
+//   let suspectThree = 'Colonel Mustard';
+//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// }
+//
+// const suspects = declareAllSuspects();
+// console.log(suspects);
+// console.log(`Suspect three is ${suspectThree}.`);
+// the first console will list the three suspects with the third being changed to colonel mustard due to the block scope taking affect. The declareAllSuspects will have lexical scope to see the first 2 suspects.
+// The second console will return mrs peacock because the scope of suspectthree is taken from the initial let declaration not the suspects constant.
+
+// Episode 5
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Kitchen',
+//   weapon: 'Candle Stick'
+// };
+//
+// const changeWeapon = function(newWeapon) {
+//   scenario.weapon = newWeapon;
+// }
+//
+// const declareWeapon = function() {
+//   return `The weapon is the ${scenario.weapon}.`;
+// }
+//
+// changeWeapon('Revolver');
+// const verdict = declareWeapon();
+// console.log(verdict);
+// the console will return 'Revolver' because the key value pairs within the object can be changed even though it is assigned as a const. the declareWeapon function returns the changed weapon and that is passed to the console through the constant verdict variable.
+
+// Episode 6
+// let murderer = 'Colonel Mustard';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+//
+//   const plotTwist = function() {
+//     murderer = 'Mrs. White';
+//   }
+//
+//   plotTwist();
+// }
+//
+// const declareMurderer = function () {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+// The murderer will be Mrs White, the constant murderer is first changed to Mr Green through the changeMurderer function, within this there is another plotTwist function assigned to a constant this has lexical scope to be able to change the murderer. this plot twist can change the murderer to Mrs White. the declare murderer function then returns the newly changed murderer constant and this is passed to the log
+
+// Episode 7
+// let murderer = 'Professor Plum';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+//
+//   const plotTwist = function() {
+//     let murderer = 'Colonel Mustard';
+//
+//     const unexpectedOutcome = function() {
+//       murderer = 'Miss Scarlet';
+//     }
+//
+//     unexpectedOutcome();
+//   }
+//
+//   plotTwist();
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+// The murderer will be Mr Green. the let murderer = colonel mustard within the plot twist function is only block scoped, so even though there is an unexpected outcome this only changes the name of the plot twist block scoped murderer. This does not affect the inital murderer which is called.
+
+// Episode 8
+// const scenario = {
+//   murderer: 'Mrs. Peacock',
+//   room: 'Conservatory',
+//   weapon: 'Lead Pipe'
+// };
+//
+// const changeScenario = function() {
+//   scenario.murderer = 'Mrs. Peacock';
+//   scenario.room = 'Dining Room';
+//
+//   const plotTwist = function(room) {
+//     if (scenario.room === room) {
+//       scenario.murderer = 'Colonel Mustard';
+//     }
+//
+//     const unexpectedOutcome = function(murderer) {
+//       if (scenario.murderer === murderer) {
+//         scenario.weapon = 'Candle Stick';
+//       }
+//     }
+//
+//     unexpectedOutcome('Colonel Mustard');
+//   }
+//
+//   plotTwist('Dining Room');
+// }
+//
+// const declareWeapon = function() {
+//   return `The weapon is ${scenario.weapon}.`
+// }
+//
+// changeScenario();
+// const verdict = declareWeapon();
+// console.log(verdict);
+
+// the murder weapon is the candle stick. None of the variables are outside the scope of each other so they can all be changed in each function. following the code through the if statements all return true which eventually results in the weapon being changed to candle stick.
+
+//Episode 9
+// let murderer = 'Professor Plum';
+//
+// if (murderer === 'Professor Plum') {
+//   let murderer = 'Mrs. Peacock';
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// const verdict = declareMurderer();
+// console.log(verdict);
+// line 175 is block scope so as soon as the if statement block ends the murderer is reverted back to professor plum.
